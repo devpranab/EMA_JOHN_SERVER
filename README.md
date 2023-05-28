c1c22c3ea83224b0d5906492b8cca89f3220423b
Pranab Sarkargit config --global user.name Pranab <dev.pranabsk@gmail.com>
2023-05-28 11:07:56 +0530

    Connect database and create post endpoint

--- README.md
+++ README.md
@@ -9,3 +9,30 @@
 - A simple introduction to mongoose
 - Module Summary and folder architecture
 - How to implement search that loads data from database
+
+
+
+
+
+//MongoDB Start
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i8jndut.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const productsCollection = client.db("emaJohnDb").collection("products");
+  // perform actions on the collection object
+  console.log("db connected");
+
+  //POST
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    productsCollection.insertOne(product)
+    .then(res => {
+        console.log(res);
+    })
+  });
+});
+//MongoDB End
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 require('dotenv').config();
 
@@ -11,6 +12,25 @@ const port = 3003;
 app.get('/', (req, res) => {
     res.send('Ema-John Server is Running')
 });
+
+// MongoDB start
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@database.1n8y8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db(`${process.env.DB_NAME}`).collection("products");
+    console.log(`MongoDB connected!`);
+    //POST
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        productsCollection.insertOne(product)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+});
+// MongoDB end
 
 app.listen(port, () => {
     console.log(`Server running on ${port}`);
